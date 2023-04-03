fc5488d1f9bd2c288de384452d782a918ddb2d66
SaulHAC <saulalamillah@gmail.com>
2023-04-03 16:56:46 -0500

    Dropdown menu para las secciones faltantes

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -61,6 +61,129 @@
                     </a>
                 </li>
             </ul>
+
+            <!--DROPDOWN-->
+            <div class="flex flex-col">
+                <div
+                    x-data="{
+                        open: false,
+                        toggle() {
+                            if (this.open) { return this.close() }
+                            this.$refs.button.focus()
+                            this.open = true
+                        },
+                        close(focusAfter) {
+                            if (! this.open) return
+                            this.open = false
+                            focusAfter && focusAfter.focus()
+                        }
+                    }"
+                    x-on:keydown.escape.prevent.stop="close($refs.button)"
+                    x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
+                    x-id="['dropdown-button']"
+                    class="relative"
+                    >
+                    <!-- Button -->
+                    <button
+                        x-ref="button"
+                        x-on:click="toggle()"
+                        :aria-expanded="open"
+                        :aria-controls="$id('dropdown-button')"
+                        type="button"
+                        class="flex items-center gap-2 p-2 rounded-md
+                        hover:bg-gray-700"
+                        >
+                        Administración de recursos
+
+                        <!-- Heroicon: chevron-down -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+
+                    <!-- Panel -->
+                    <div
+                        x-ref="panel"
+                        x-show="open"
+                        x-transition.origin.top.left
+                        x-on:click.outside="close($refs.button)"
+                        :id="$id('dropdown-button')"
+                        style="display: none;"
+                        class="left-0 mt-2 rounded-md"
+                        >
+                        <a href="#" class="flex items-center gap-2 w-full rounded-md px-4 py-2.5 text-left text-sm hover:bg-gray-700 disabled:text-gray-500">
+                            Cultura organizacional
+                        </a>
+
+                        <a href="#" class="flex items-center gap-2 w-full rounded-md px-4 py-2.5 text-left text-sm hover:bg-gray-700 disabled:text-gray-500">
+                            Estructura legal
+                        </a>
+                    </div>
+                </div>
+
+                <!--Segundo dropdown-->
+                <div
+                    x-data="{
+                        open: false,
+                        toggle() {
+                            if (this.open) { return this.close() }
+                            this.$refs.button.focus()
+                            this.open = true
+                        },
+                        close(focusAfter) {
+                            if (! this.open) return
+                            this.open = false
+                            focusAfter && focusAfter.focus()
+                        }
+                    }"
+                    x-on:keydown.escape.prevent.stop="close($refs.button)"
+                    x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
+                    x-id="['dropdown-button']"
+                    class="relative"
+                    >
+                    <!-- Button -->
+                    <button
+                        x-ref="button"
+                        x-on:click="toggle()"
+                        :aria-expanded="open"
+                        :aria-controls="$id('dropdown-button')"
+                        type="button"
+                        class="flex items-center gap-2 p-2 rounded-md
+                        hover:bg-gray-700"
+                        >
+                        Imagen corporativa y marketing
+
+                        <!-- Heroicon: chevron-down -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+
+                    <!-- Panel -->
+                    <div
+                        x-ref="panel"
+                        x-show="open"
+                        x-transition.origin.top.left
+                        x-on:click.outside="close($refs.button)"
+                        :id="$id('dropdown-button')"
+                        style="display: none;"
+                        class="left-0 mt-2 rounded-md"
+                        >
+                        <a href="#" class="flex items-center gap-2 w-full rounded-md px-4 py-2.5 text-left text-sm hover:bg-gray-700 disabled:text-gray-500">
+                            Imagen de la empresa
+                        </a>
+
+                        <a href="#" class="flex items-center gap-2 w-full rounded-md px-4 py-2.5 text-left text-sm hover:bg-gray-700 disabled:text-gray-500">
+                            Publicidad y promoción
+                        </a>
+
+                        <a href="#" class="flex items-center gap-2 w-full rounded-md px-4 py-2.5 text-left text-sm hover:bg-gray-700 disabled:text-gray-500">
+                            Plan de mercadotecnia
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!---->
         </aside>
     </div>
 </div>
