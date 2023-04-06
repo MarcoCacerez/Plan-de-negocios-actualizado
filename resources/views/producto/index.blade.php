1f202f91fcecfc54a39b1e3ff08bf15bd720dae3
SaulHAC <saulalamillah@gmail.com>
2023-04-06 01:18:58 -0500

    cambios en la paginacion y aspecto de product/serv

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -2,16 +2,16 @@
 
     <x-sidebar :plan_de_negocio="$plan_de_negocio"></x-sidebar>
     <div class="w-full">
-        <div class="flex items-center justify-center">
-            <h1 class="dark:text-gray-100 py-6 text-2xl">Producto y/o servicio</h1>
+        <div class="mx-20 flow-root">
+            <h1 class="float-left dark:text-gray-100 my-6 text-2xl">Producto y/o servicio</h1>
+            <a href="{{ route('plan_de_negocio.producto.create', [$plan_de_negocio]) }}" class="float-right my-6 mb-4 inline-flex items-center px-4 py-2 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Agregar producto
+            </a>
         </div>
 
         <div class="mx-20"> 
-            <a href="{{ route('plan_de_negocio.producto.create', [$plan_de_negocio]) }}" class="mb-4 inline-flex items-center px-4 py-2 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                Agregar producto
-            </a>
 
-            <div class="flex relative overflow-x-auto shadow-md sm:rounded-lg">
+            <div class="flex my-2 relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -48,8 +48,15 @@
                                     {{ $producto->descripcion }}
                                 </td>
                                 <td class="px-6 py-4">
-                                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar</a>
-                                    <a href="#" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-blue-800">Borrar</a>    
+                                    <div class="">
+                                        <a href="#" class="inline-flex items-center px-1.5 py-1.5 bg-blue-700 hover:bg-blue-800 text-white text-sm font-medium rounded-md">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path><polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon></svg>
+                                        </a>
+
+                                        <a href="#" class="inline-flex items-center px-1.5 py-1.5 bg-red-700 hover:bg-red-800 text-white text-sm font-medium rounded-md">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                                        </a>
+                                    </div>
                                 </td>
                             </tr>
                         @endforeach
